4cc8635b5c5e6e7efc246994db494cce009761ba
Alonso Rosales <a20191566@pucp.edu.pe>
2022-11-08 12:49:04 -0500

    registro 50%

--- app/src/main/java/com/example/tpo/RegistroActivity.java
+++ app/src/main/java/com/example/tpo/RegistroActivity.java
@@ -1,14 +1,119 @@
 package com.example.tpo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class RegistroActivity extends AppCompatActivity {
+    String correo;
+    String contrasena;
+    String confirmarContrasena;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Button botonRegistro = findViewById(R.id.botonRegistro);
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean guardar = true;
+
+                try {
+
+                    EditText correoText = findViewById(R.id.correoRegistro);
+                    String correoHelper = correoText.getText().toString();
+                    if (correoHelper.equalsIgnoreCase("") || correoHelper == null || correoHelper.isEmpty()) {
+                        correoText.setError("Ingrese un correo válido");
+                        guardar = false;
+
+                    } else {
+                        if (correoHelper.contains("@")) {
+                            String[] partesCorreo = correoHelper.split("@");
+
+                            if (partesCorreo[0].equals("")) {
+                                correoText.setError("Ingrese un correo válido");
+                                guardar = false;
+                            }
+
+                            if (!partesCorreo[1].equals("pucp.edu.pe")) {
+                                correo = correoHelper;
+
+                            } else {
+                                correoText.setError("No se permite correos PUCP");
+                                guardar = false;
+                            }
+
+                        } else {
+                            correoText.setError("Ingrese un correo válido");
+                            guardar = false;
+                        }
+                    }
+
+
+                    TextView contraText = findViewById(R.id.contraRegistro);
+                    contrasena = contraText.getText().toString();
+                    if (contrasena.equalsIgnoreCase("") || contrasena == null || contrasena.isEmpty()) {
+                        contraText.setError("Ingrese su contraseña");
+                        guardar = false;
+                    }
+
+                    TextView recontraText = findViewById(R.id.recontraRegistro);
+                    confirmarContrasena = recontraText.getText().toString();
+                    if (confirmarContrasena.equalsIgnoreCase("") || confirmarContrasena == null || confirmarContrasena.isEmpty()) {
+                        recontraText.setError("Ingrese su contraseña");
+                        guardar = false;
+                    }
+
+                    if (!contrasena.equals(confirmarContrasena)) {
+                        guardar = false;
+                        recontraText.setError("Las contraseñas no coinciden");
+                    }
+
+
+                    if(guardar){
+                        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                        firebaseAuth.createUserWithEmailAndPassword(correo,contrasena).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegistroActivity.this,"Cuenta creada exitosamente!",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(RegistroActivity.this,"Contraseñas incorrectas!",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+                    }else{
+                        Toast.makeText(RegistroActivity.this,"Campo(s) incorrecto(s)!",Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }catch (Exception e){
+                    Toast.makeText(RegistroActivity.this,"Campo(s) incorrecto(s)!",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
     }
 }
